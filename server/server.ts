6e4898c58bcf299fe2b3eb7f2ebe2b6108909bc7
I-am-Jay-Singh <br20f5604@gmail.com>
2026-02-27 16:59:39 +0530

    added cors origin

--- server/server.ts
+++ server/server.ts
@@ -24,7 +24,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://thumblify-client-sage.vercel.app",
+      "https://thumblify-client-nu.vercel.app",
     ],
     credentials: true,
   }),
